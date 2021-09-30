b108f60ea1f9a164b7670a2d5e5d093b39b46311
BikramNegi <bikramnegi91@gmail.com>
2021-10-01 00:45:56 +0530

    ADD: Created User Routes And Models

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const UserRoute = require("./routes/UserRoute")
+const AuthRoute = require("./routes/AuthRoute")
 
+//Environment Configuration
 dotenv.config()
 
+//App Initialization
 const app = express()
+console.log("Server start")
 
+//Middleware to use JSON in app
+app.use(express.json())
+
+//Routes
+app.use("/api/auth", AuthRoute)
+app.use("/api/users", UserRoute)
+
+//Database Connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successfull"))
   .catch((err) => console.log(err))
 
-app.listen(process.env.PORT || 5000, () => {
+// Server initialization
+app.listen(process.env.PORT || 5000, (error) => {
+  if (error) {
+    console.log(error)
+  }
   console.log("Backend server is running")
 })
